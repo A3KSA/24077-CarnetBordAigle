4265d75dfb484328460480addc906177135859ca
romain.chassot <romain.chassot@automation3000.ch>
2025-01-06 11:56:34 +0100

    Update require model user

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user'); // Utilise 'user' avec un "u" minuscule
 
 exports.protect = async (req, res, next) => {
   let token;
